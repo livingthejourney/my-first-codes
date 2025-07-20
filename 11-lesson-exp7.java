95385494e8622054c8f7e596f358e0a1e9abaaad
livingthejourney <asenamutlu95@gmail.com>
2025-07-20 18:45:56 +0300

    Create 11-lesson-exp7.java

---
+++ 11-lesson-exp7.java
@@ -0,0 +1,10 @@
+public class Main
+{
+	public static void main(String[] args) {
+	//20'den küçük pozitif tam sayıları ekrana yazdıran program.
+	int i;
+	for (i = 0; i < 20; i = i + 2)
+	    System.out.print(i + " ");
+
+	}
+}
