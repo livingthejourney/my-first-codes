3bb2f8e0999e30b9c29dc84ed44951732dac348c
livingthejourney <asenamutlu95@gmail.com>
2025-07-20 18:43:05 +0300

    Create 11-lesson-exp5.java
    for

---
+++ 11-lesson-exp5.java
@@ -0,0 +1,12 @@
+public class Main
+{
+	public static void main(String[] args) {
+	//1'den 10'a kadar olan sayıları ekrana yazdıran program.
+	
+	int i;
+	
+	for (i = 1; i <=10; i = i + 1)
+	    System.out.print(i + " ");
+	    
+	}
+}
