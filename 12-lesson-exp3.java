f0c74b67b4dd33d86f15ad7df3347e725c052e6d
livingthejourney <asenamutlu95@gmail.com>
2025-07-20 19:14:18 +0300

    Update 12-lesson-exp3.java

--- 12-lesson-exp3.java
+++ 12-lesson-exp3.java
@@ -1,10 +1,16 @@
 public class Main
 {
 	public static void main(String[] args) {
-	//20'den küçük pozitif tam sayıları ekrana yazdıran program.
-	int i;
-	for (i = 0; i < 20; i = i + 2)
+	// klavyeden a ve b giriliyor. a 'nın b 'den küçük girildiği kabul ediliyor.(a < b) a 'dan b ye kadar olan sayıları ekrana yazdıran program.
+	java.util.Scanner kb = new java.util.Scanner(System.in);
+	int a,b,i;
+	System.out.print("a: ");
+	a = kb.nextInt();
+	System.out.print("b: ");
+	b = kb.nextInt();
+	
+	for (i= a; i <= b; i = i + 1)
 	    System.out.print(i + " ");
-
+	    
 	}
 }
