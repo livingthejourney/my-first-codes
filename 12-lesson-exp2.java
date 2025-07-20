44de98b35f99dfe25902547aca0a4499c356051c
livingthejourney <asenamutlu95@gmail.com>
2025-07-20 19:09:10 +0300

    Update 12-lesson-exp2.java

--- 12-lesson-exp2.java
+++ 12-lesson-exp2.java
@@ -1,12 +1,13 @@
 public class Main
 {
 	public static void main(String[] args) {
-	//1'den 10'a kadar olan sayıları ekrana yazdıran program.
-	
-	int i;
-	
-	for (i = 1; i <=10; i = i + 1)
+	// n klavyeden girilen pozitif bir sayıdır. 1'den n 'ye kadar olan sayıları ekrana yazdıran program.
+	java.util.Scanner kb = new java.util.Scanner(System.in);
+	int i,n;
+	System.out.print("n: ");
+	n = kb.nextInt();
+	 
+	for (i = 1; i < n; i = i + 1)
 	    System.out.print(i + " ");
-	    
 	}
 }
