2d30e05a6aab6434cbe7a5ecb13bc9204a64b051
livingthejourney <asenamutlu95@gmail.com>
2025-07-20 19:04:29 +0300

    Update 12-lesson-exp1.java

--- 12-lesson-exp1.java
+++ 12-lesson-exp1.java
@@ -1,1 +1,11 @@
+public class Main
+{
+	public static void main(String[] args) {
+	//10'dan geriye doğru (1 dahil) sayıları ekrana yazdıran program.
+	int i;
+    for (i = 10; i >= 1; i = i -1)
+        System.out.print(i + " ");
+
+	}
+}
 
