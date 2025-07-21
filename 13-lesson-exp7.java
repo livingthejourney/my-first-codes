28a2561655c6526d491cf8f8f28835fb70c9c3da
livingthejourney <asenamutlu95@gmail.com>
2025-07-21 18:01:18 +0300

    Update 13-lesson-exp7.java

--- 13-lesson-exp7.java
+++ 13-lesson-exp7.java
@@ -3,7 +3,7 @@ public class Main
 	public static void main(String[] args) {
   //1 ile 100 sayısı arasındaki 3,4 ve 5’in katlarını ekrana iç içe yazdıran program.
     int i;
-  for (i = 1; i <100; i = i + 1) {
+  for (i = 1; i <= 100; i = i + 1) {
     if ( i % 3 == 0)
         System.out.print(i + " ");
     if ( i % 4 == 0)
