d56a9645e5090af8793a78691d914063b31f9a5e
livingthejourney <asenamutlu95@gmail.com>
2025-07-21 17:59:40 +0300

    Create 13-lesson-exp7.java
    for and if

---
+++ 13-lesson-exp7.java
@@ -0,0 +1,16 @@
+public class Main
+{
+	public static void main(String[] args) {
+  //1 ile 100 sayısı arasındaki 3,4 ve 5’in katlarını ekrana iç içe yazdıran program.
+    int i;
+  for (i = 1; i <100; i = i + 1) {
+    if ( i % 3 == 0)
+        System.out.print(i + " ");
+    if ( i % 4 == 0)
+        System.out.print(i + " ");
+    if ( i % 5 == 0)
+        System.out.print(i + " ");
+  } 
+   
+	}
+}
