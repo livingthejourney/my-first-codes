5cece727cde5fbac4bfff3eaa6ca1aa81ec6bcb5
livingthejourney <asenamutlu95@gmail.com>
2025-07-22 14:39:17 +0300

    Create 14-lesson-exp5.java

---
+++ 14-lesson-exp5.java
@@ -0,0 +1,18 @@
+public class Main
+{
+	public static void main(String[] args) {
+    //Klavyeden girilen sayıdan küçük dizi elemanlarını ekrana yazdıran program. 
+    
+    int a[] = {4, 8, 3, 1, 18, 9, 21, 20, 5, 17};
+    java.util.Scanner kb = new java.util.Scanner(System.in);
+    int n,i,size;
+    size = 10;
+    
+    System.out.print("n: ");
+    n = kb.nextInt();
+    for (i = 0; i < size; i = i +1 )
+        if (a[i] < n)
+            System.out.print(a[i] + " ");
+            
+	}
+}
